7d3195d4ae25127343a47551e1b6648882589106
mongol <verywisemongol@gmail.com>
2018-04-16 10:57:46 +0300

    initial commit

---
+++ src/main/java/kharkov/kp/gic/dossier/DossierApplication.java
@@ -0,0 +1,12 @@
+package kharkov.kp.gic.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierApplication.class, args);
+	}
+}
